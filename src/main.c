bc18c8b7128e5537709cdccde4f46db700ad77da
Chris Shifrin <chris.shifrin7@gmail.com>
2022-01-15 15:40:57 -0500

    formatting

--- src/main.c
+++ src/main.c
@@ -148,14 +148,14 @@ printf("\n\
         {
             uint8_t pdata[16];
             uint8_t count = 0;
+            uint16_t addr = cmd[13] | cmd[14];
             for(uint8_t x = 16; x < 32; x++)
             {
                 pdata[count] = cmd[x];
-                printf("%c", pdata[count]);
-                nrf_delay_ms(3);
+                nrf_delay_ms(1);
             }
             uint8_t data[16] = {cmd[16], cmd[17], cmd[18], cmd[19], cmd[20], cmd[21], cmd[22], cmd[23], cmd[24], cmd[25], cmd[26], cmd[27], cmd[28], cmd[29], cmd[30], cmd[31]};
-            eeprom_cmd_write(1, data);
+            nrf_delay_ms(2);
         }
 
         else
